e766f7a341853830375faf6de3d409056a5f82c5
Carlos Marin <33185069+carlosmarin88@users.noreply.github.com>
2024-08-08 17:47:21 -0300

    Agregar sweetalert2 + redux-toolkit

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -1,9 +1,11 @@
 import { addHours, differenceInSeconds } from 'date-fns';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import Modal from 'react-modal';
 import DatePicker, { registerLocale } from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { es } from 'date-fns/locale/es';
+import Swal from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 registerLocale('es', es);
 
@@ -24,12 +26,22 @@ Modal.setAppElement('#root');
 export const CalendarModal = () => {
 
     const [isOpen, setIsOpen] = useState(true);
+    const [formSubmitted, setFormSubmitted] = useState(false);
+
     const [formValues, setFormValues] = useState({
         title: 'Carlos',
         notes: 'Marin',
         start: new Date(),
         end: addHours(new Date(), 2)
     });
+
+    const titleClass = useMemo(() => {
+
+        if (!formSubmitted) return '';
+        return (formValues.title.length > 0) ? '' : 'is-invalid';
+
+
+    }, [formValues.title, formSubmitted])
 
     const onInputChanged = ({ target }) => {
         setFormValues({
@@ -46,20 +58,22 @@ export const CalendarModal = () => {
     }
 
     const oncCloseModal = () => {
-        console.log('close modal');
+        //console.log('close modal');
         setIsOpen(false);
     }
 
     const onSubmit = (event) => {
         event.preventDefault();
+        setFormSubmitted(true);
         const difference = differenceInSeconds(formValues.end, formValues.start);
-        //console.log({difference});
-        if(isNaN(difference) || difference > 0) {
-            console.log('error con las fechas')
+       // console.log({ difference });
+        if (isNaN(difference) || difference <= 0) {
+            //console.log('error con las fechas');
+            Swal.fire('Fecha incorrestas', 'Revisar las fechas ingresadas.', 'error');
             return;
         }
 
-        if(formValues.title.length <= 0) return;
+        if (formValues.title.length <= 0) return;
 
         console.log(formValues);
 
@@ -116,7 +130,7 @@ export const CalendarModal = () => {
                     <label>Titulo y notas</label>
                     <input
                         type="text"
-                        className="form-control"
+                        className={`form-control ${titleClass}`}
                         placeholder="Título del evento"
                         name="title"
                         autoComplete="off"
